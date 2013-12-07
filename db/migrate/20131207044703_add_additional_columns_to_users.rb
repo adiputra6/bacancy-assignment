2aaf7016afe6de8d2834b5f1a0768417a564a963
madhu <madhu@madhu-Lenovo-G560.(none)>
2013-12-07 10:32:38 +0530

    first commit

---
+++ db/migrate/20131207044703_add_additional_columns_to_users.rb
@@ -0,0 +1,10 @@
+class AddAdditionalColumnsToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :address, :string
+    add_column :users, :mobile, :string
+    add_column :users, :country, :string
+    add_column :users, :state, :string
+    add_column :users, :city, :string
+    add_column :users, :gender, :boolean
+  end
+end
